78f9c3af468765da93b75e445a64e470101e7746
Benjamin Charlton <kuldrgames@gmail.com>
2018-10-19 14:12:54 +0100

    Extended the definition of merge including a comment

--- PartI/Code/Hamming.hs
+++ PartI/Code/Hamming.hs
@@ -1,3 +1,7 @@
+-- merge takes in 2 lists of the same type and sorts them in asscending order with no duplicates
+merge :: Ord a => [a] -> [a] -> [a]
 merge xxs@(x:xs) yys@(y:ys) | x == y = x : merge xs ys
                             | x <  y = x : merge xs yys
                             | x >  y = y : merge xxs ys
+merge xxs        []                  = xxs
+merge []          yys                = yys
