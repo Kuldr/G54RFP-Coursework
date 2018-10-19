9f660bc8d89525f515854f94aadd4b66c9a36246
Benjamin Charlton <kuldrgames@gmail.com>
2018-10-19 14:29:22 +0100

    Added way to generate next hamming numbers from any n

--- PartI/Code/Hamming.hs
+++ PartI/Code/Hamming.hs
@@ -6,6 +6,13 @@ hamming20 = [1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,25,27,30,32,36]
 hamming :: Num a => [a]
 hamming = []
 
+-- hammingNext takes in a number n and returns the list of the hamming numbers directly generated from n and n itself
+hammingNext :: Num a => a -> [a]
+hammingNext n = [n, x, y, z]
+                    where x = 2*n
+                          y = 3*n
+                          z = 5*n
+
 -- merge takes in 2 lists of the same type and sorts them in asscending order with no duplicates
 merge :: Ord a => [a] -> [a] -> [a]
 merge xxs@(x:xs) yys@(y:ys) | x == y = x : merge xs ys
