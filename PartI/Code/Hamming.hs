ea6ecfcc62aed2303326c346510dd7dced4270d5
Benjamin Charlton <kuldrgames@gmail.com>
2018-10-19 14:16:21 +0100

    Added a list containing the first 20 hamming numbers

--- PartI/Code/Hamming.hs
+++ PartI/Code/Hamming.hs
@@ -1,3 +1,7 @@
+--hamming20 is the first 20 hamming numbers
+hamming20 :: Num a => [a]
+hamming20 = [1,2,3,4,5,6,8,9,10,12,15,16,18,20,24,25,27,30,32,36]
+
 -- merge takes in 2 lists of the same type and sorts them in asscending order with no duplicates
 merge :: Ord a => [a] -> [a] -> [a]
 merge xxs@(x:xs) yys@(y:ys) | x == y = x : merge xs ys
