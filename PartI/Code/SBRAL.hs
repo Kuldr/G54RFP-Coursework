e8e27a443378b24eea740ae17a737f57a97df83e
Benjamin Charlton <kuldrgames@gmail.com>
2018-11-05 14:56:49 +0000

    done skew binary trees

--- PartI/Code/SBRAL.hs
+++ PartI/Code/SBRAL.hs
@@ -8,6 +8,19 @@ data Tree a = Leaf a | Node (Tree a) a (Tree a) deriving Show
 type RList a = [(Int, Tree a)]
 
 
+delete :: Int -> RList a -> RList a
+delete i ((w, t) : wts) | i < w     = deleteTree i w t ++ wts
+                        | otherwise = delete (i - w) wts
+
+deleteTree :: Int -> Int -> Tree a -> RList a
+deleteTree i _ (Leaf x) | i /= 0    = []
+                        | otherwise = [(1, Leaf x)]
+deleteTree i w (Node t1 x t2) | i == 0    = [(w, (Node t1 x t2))]
+                              | i <= w'   = deleteTree (i - 1) w' t1 ++ [(w', t2)]
+                              | otherwise = deleteTree (i - w' - 1) w' t2
+                                    where
+                                        w' = w `div` 2
+
 empty :: RList a
 empty = []
 
@@ -17,8 +30,7 @@ isEmpty ts = null ts
 
 
 cons :: a -> RList a -> RList a
-cons x ((w1, t1) : (w2, t2) : wts) | w1 == w2 =
-    (w1 * 2 + 1, Node t1 x t2) : wts
+cons x ((w1, t1) : (w2, t2) : wts) | w1 == w2 = (w1 * 2 + 1, Node t1 x t2) : wts
 cons x wts = ((1, Leaf x) : wts)
 
 
@@ -61,3 +73,12 @@ updateTree i x w (Node t1 y t2)
     | otherwise = Node t1 y (updateTree (i - w' - 1) x w' t2)
     where
         w' = w `div` 2
+
+list3 :: RList Int
+list3 = [(3, Node (Leaf 1) 2 (Leaf 3)) ]
+
+list2 :: RList Int
+list2 = [(1, Leaf 1), (1, Leaf 2), (7, Node (Node (Leaf 3) 4 (Leaf 5)) 6 (Node (Leaf 7) 8 (Leaf 9)))]
+
+list4 :: RList Int
+list4 = [(1,Leaf 1),(1,Leaf 2),(7,Node (Node (Leaf 5) 4 (Leaf 6)) 3 (Node (Leaf 8) 7 (Leaf 9)))]
