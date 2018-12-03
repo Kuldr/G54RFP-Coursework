697bda36304913feddcb1852f447c03234a52307
Benjamin Charlton <kuldrgames@gmail.com>
2018-12-03 15:12:38 +0000

    Changed spelling mistake in philosopher

--- PartII/Code/DiningPhilosophers.hs
+++ PartII/Code/DiningPhilosophers.hs
@@ -16,70 +16,70 @@ getTimeString = do
                     zt <- getZonedTime
                     return $ formatTime defaultTimeLocale "%H:%M:%S" zt
 
--- Prints to screen "Time | Philosipher Action"
-printPhilosipherAction :: String -> String -> IO ()
-printPhilosipherAction name a = do
+-- Prints to screen "Time | Philosopher Action"
+printPhilosopherAction :: String -> String -> IO ()
+printPhilosopherAction name a = do
                                     timeString <- getTimeString
                                     putStrLn (timeString ++ " | " ++ name ++ a)
 
--- Prints to screen "Time | Delaying Philosipher s Seconds" and delays the thread
-printPhilosipherDelay :: String -> Int -> IO ()
-printPhilosipherDelay name s = do
+-- Prints to screen "Time | Delaying Philosopher s Seconds" and delays the thread
+printPhilosopherDelay :: String -> Int -> IO ()
+printPhilosopherDelay name s = do
                                     timeString <- getTimeString
                                     putStrLn (timeString ++ " | Delaying " ++ name ++ " " ++ show(s) ++ " seconds")
                                     threadDelay (μstos s)
-                                    
+
 -- Doesn't need to be TMVar Int
 
-philosiphers :: [String]
-philosiphers = ["Socrates", "Kant", "Aristotle", "Descartes", "Plato", "Aquinas", "Marx"]
+philosophers :: [String]
+philosophers = ["Socrates", "Kant", "Aristotle", "Descartes", "Plato", "Aquinas", "Marx"]
 
-philosipher :: String -> ((TMVar Int), (TMVar Int)) -> IO ()
-philosipher name (ls,rs) = do
-    -- Philosipher is thinking
+philosopher :: String -> ((TMVar Int), (TMVar Int)) -> IO ()
+philosopher name (ls,rs) = do
+    -- Philosopher is thinking
     -- Wait for a random number of seconds and announce you are doing so
-    printPhilosipherAction name " is thinking"
+    printPhilosopherAction name " is thinking"
     seconds <- randomRIO (1, 10)
-    printPhilosipherDelay name seconds
+    printPhilosopherDelay name seconds
 
-    -- Philosipher is hungry
+    -- Philosopher is hungry
     -- Attempt to get both sporks together (will wait until it can get both)
-    printPhilosipherAction name " is hungry"
+    printPhilosopherAction name " is hungry"
     (l, r) <- atomically $ do
         l <- takeTMVar ls
         r <- takeTMVar rs
         return (l, r)
 
-    -- Philosipher is eating
-    -- Philosipher has both sporks so they can eat for a random number of seconds
-    printPhilosipherAction name " is eating"
+    -- Philosopher is eating
+    -- Philosopher has both sporks so they can eat for a random number of seconds
+    printPhilosopherAction name " is eating"
     seconds <- randomRIO (1, 10)
-    printPhilosipherDelay name seconds
+    printPhilosopherDelay name seconds
 
-    -- Philosipher has finished eating so put both forks back together.
+    -- Philosopher has finished eating so put both forks back together.
     atomically $ do
         putTMVar ls l
         putTMVar rs r
 
-    -- Loop the philosipher so they are thinking again
-    philosipher name (ls,rs)
+    -- Loop the philosopher so they are thinking again
+    philosopher name (ls,rs)
 
 main = do
-    -- Create a list of sporks for the philosiphers to use
+    -- Create a list of sporks for the philosophers to use
     -- Used sporks as to not confuse with threads forking
-    sporks <- mapM newTMVarIO [1..length(philosiphers)]
-    -- Create a pair of sporks left and right of a philosipher
+    sporks <- mapM newTMVarIO [1..length(philosophers)]
+    -- Create a pair of sporks left and right of a philosopher
     let pairs = zip sporks (tail(sporks) ++ [(head sporks)])
 
-    -- Fork all of the philosiphers with the a name and a pair
-    forkIO (philosipher (philosiphers!!0) (pairs!!0))
-    forkIO (philosipher (philosiphers!!1) (pairs!!1))
-    forkIO (philosipher (philosiphers!!2) (pairs!!2))
-    forkIO (philosipher (philosiphers!!3) (pairs!!3))
-    forkIO (philosipher (philosiphers!!4) (pairs!!4))
-    forkIO (philosipher (philosiphers!!5) (pairs!!5))
-    forkIO (philosipher (philosiphers!!6) (pairs!!6))
+    -- Fork all of the philosophers with the a name and a pair
+    forkIO (philosopher (philosophers!!0) (pairs!!0))
+    forkIO (philosopher (philosophers!!1) (pairs!!1))
+    forkIO (philosopher (philosophers!!2) (pairs!!2))
+    forkIO (philosopher (philosophers!!3) (pairs!!3))
+    forkIO (philosopher (philosophers!!4) (pairs!!4))
+    forkIO (philosopher (philosophers!!5) (pairs!!5))
+    forkIO (philosopher (philosophers!!6) (pairs!!6))
 
-    -- Used to delay main forever so that the philosiphers can run
+    -- Used to delay main forever so that the philosophers can run
     -- Need to use Ctrl+C to close the program
     forever $ threadDelay (μstos 1)
